c57dfe58ca19619784e76318463a8dcb50db615d
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-28 20:05:12 +0600

    key-value pairs with their types

---
+++ problem5.js
@@ -0,0 +1,13 @@
+let myObject = {
+
+name: 'Md Sakibur Rahman',
+age: 23,
+city: 'Meherpur',
+isStudent: true
+};
+for (let key in myObject) {
+    // const element = myObject[key];
+    console.log("Key:",key,'|',"type:",typeof myObject[key])
+}
+
+
