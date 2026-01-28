11787e4d073a3a82565fa5d029002807cf73fc27
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-28 19:28:46 +0600

    Object length

---
+++ problem4.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Md Sakibur Rahman',
+    age: 23,
+    city: 'Meherpur',
+    isStudent: true,
+    isPassed: true
+};
+
+let Len = Object.keys(student).length;
+console.log(Len);
